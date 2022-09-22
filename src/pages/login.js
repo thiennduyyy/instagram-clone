8aefa441d0d11246ead42dc82e444d1d896fc1fc
unknown <thienduyyy@gmail.com>
2022-09-22 12:48:01 +0700

    update login and signup pages CSS

--- src/pages/login.js
+++ src/pages/login.js
@@ -25,7 +25,7 @@ export default function Login() {
         document.title = 'Login - Instagram'
     }, [])
     return (
-        <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+        <div className='container flex mx-auto max-w-desktop items-center h-screen'>
             <div className="flex w-3/5">
                 <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
             </div>
